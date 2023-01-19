8caa599975c8638709132ab037a9c9e970c089a3
gitVeron <98803425+gitVeron@users.noreply.github.com>
2023-01-20 00:51:24 +0300

    Update ComicsList.js

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -24,6 +24,7 @@ const ComicsList = () => {
     }
 
     const onComicsLoaded = (comicsList) => {
+        let ended = false;
         setComicsList(comicsList);
         setNewItemLoading(false);
         setOffset(offset => offset + 8);
